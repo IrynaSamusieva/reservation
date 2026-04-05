3d1f9cdadf80a325ed95f40d994b6c4df1f10901
Chmo <chmo123@gmail.com>
2026-04-05 13:38:44 +0200

    move target to src

--- src/main/java/iryna/samusieva/reservation_system/Reservation.java
+++ src/main/java/iryna/samusieva/reservation_system/Reservation.java
@@ -5,4 +5,5 @@ import java.time.LocalDate;
 public record Reservation(Long id, Long userId, Long roomId,
                           LocalDate startDate, LocalDate endDate,
                           ReservationStatus status)  {
+
 }
