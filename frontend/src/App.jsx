df4f36dbbff368abfffe56b6cfb6f6f89d8bde54
Chmo <chmo123@gmail.com>
2026-05-02 00:15:34 +0200

    add react files

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import ReservationGetDetail from "./controllers/ReservationGetDetail.jsx";
+
+function  App(){
+    return (
+        <div className="App">
+            <ReservationGetDetail />
+        </div>
+    );
+}
+export default App;
